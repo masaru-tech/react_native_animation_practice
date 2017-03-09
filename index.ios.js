deee27a013164e404962f274b9d7513c7b3a5f0f
masaru-tech <masaru.tech@gmail.com>
2017-03-09 11:17:21 +0900

    Qiitaで解説あったうちの１つをコピー
    ページは下記
    http://qiita.com/imaimiami/items/1ad4170c06b1922604dd

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,9 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Animated,
+  TouchableWithoutFeedback
 } from 'react-native';
 import {TabNavigator, StackNavigator} from 'react-navigation';
 
@@ -47,8 +49,46 @@ class AllContactsScreen extends Component {
 }
 
 class ChatScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yellowScale: new Animated.Value(1),
+    };
+  }
+
+  componentDidMount() {
+    this.state.yellowScale.addListener((value) => {
+      this._yellowScale = value.value
+    });
+    this.state.yellowScale.setValue(1);
+  }
+
+  _onPressYellow() {
+    Animated.spring(
+      this.state.yellowScale,
+      {
+        toValue: this._yellowScale - 0.1,
+        friction: 1,
+      }
+    ).start();
+  }
+
   render() {
-    return <Text>Chat View</Text>
+    return(
+      <View style={styles.container}>
+        <Text>Chat View</Text>
+        <TouchableWithoutFeedback onPress={this._onPressYellow.bind(this)}>
+          <Animated.View style={{
+              backgroundColor: "yellow",
+              width: 100,
+              height: 100,
+              transform: [
+                {scale: this.state.yellowScale},
+              ]}}>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
   }
 }
 
@@ -62,5 +102,12 @@ const App = StackNavigator({
   Chat: { screen: ChatScreen },
 });
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 AppRegistry.registerComponent('react_native_animation_practice', () => App);
