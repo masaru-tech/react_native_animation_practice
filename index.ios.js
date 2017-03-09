07a2100c05219c85e78469e237b441e98645ae16
masaru-tech <masaru.tech@gmail.com>
2017-03-09 10:32:59 +0900

    react-navigationのTabNavigator,StackNavigatorの入れ子サンプル

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,58 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import {TabNavigator, StackNavigator} from 'react-navigation';
 
-export default class react_native_animation_practice extends Component {
+class RecentChatsScreen extends Component {
+  static navigationOptions = {
+    header: (navigation, defaultHeader) => ({
+      ...defaultHeader,
+      visible: true,
+      tintColor: 'white', // ヘッダーの文字色
+      style: {
+        backgroundColor: 'blue' // ヘッダーの背景色
+      },
+      title: 'Page1',
+    }),
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+    return(
+      <View>
+        <Text>List of recent chats</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('Chat', { user: 'Lucy' })}
+          title="Chat with Lucy"
+        />
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+class AllContactsScreen extends Component {
+  render() {
+    return <Text>List of all contacts</Text>
+  }
+}
+
+class ChatScreen extends Component {
+  render() {
+    return <Text>Chat View</Text>
+  }
+}
+
+const MainScreenNavigator = TabNavigator({
+  Recent: { screen: RecentChatsScreen },
+  All: { screen: AllContactsScreen },
 });
 
-AppRegistry.registerComponent('react_native_animation_practice', () => react_native_animation_practice);
+const App = StackNavigator({
+  Home: { screen: MainScreenNavigator },
+  Chat: { screen: ChatScreen },
+});
+
+
+AppRegistry.registerComponent('react_native_animation_practice', () => App);
